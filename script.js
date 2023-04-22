008d7673bce1b6980990cc72e96a2df41c67690b
Daniil-Pavlichenko2002 <pavlichenko.02@internet.ru>
2023-04-22 21:04:18 +0300

    Готово

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ document.querySelector('.header__burger').addEventListener('click', function() {
     document.querySelector('.header__meny').classList.toggle('open');
     document.querySelector('body').classList.toggle('noscroll');
     document.querySelector('.footer').classList.toggle('footer__fixed');
-    document.querySelector('.services__button').classList.toggle('button__fixed');
-
-    // if(document.footer.style.display = 'block'){
-    //     document.footer.style.overflow = 'hidden'
-    // } else {
-    //     document.body.style.overflow = 'auto'
-    // }
-    // document.querySelector('.wrapper').classList.toggle('shadow')
+    document.querySelector('.header__shadow').classList.toggle('open');
+    });
+    document.querySelector('.header__shadow').addEventListener('click', function() {
+        document.querySelector('.header__shadow').classList.remove('open');
+        document.querySelector('.header__meny').classList.toggle('open');
+        document.querySelector('.header__burger').classList.remove('active');
+        document.querySelector('body').classList.remove('noscroll');
+        document.querySelector('.footer').classList.remove('footer__fixed');
+        document.querySelector('.project__buttom').classList.remove('button__fixed');
     })
-// document.querySelector('body').addEventListener('click', function(){
-//     document.header__meny.style.display = 'none'
-//     document.querySelector('.header__meny').classList.add('body__click');
-// })
+  
+const shadow = document.querySelector('.header__shadow');
+console.log(shadow);
